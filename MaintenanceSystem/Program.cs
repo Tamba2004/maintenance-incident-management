fe95b8bb65cc4cb6d2ccb99d67f3dbc6820ebe3b
Alan Soh <alansoh343@gmail.com>
2026-05-12 08:54:21 -0400

    Setup backend architecture with EF Core and incident service

--- MaintenanceSystem/Program.cs
+++ MaintenanceSystem/Program.cs
@@ -1,6 +1,7 @@
 using MaintenanceSystem.Components;
 using MaintenanceSystem.Data;
 using Microsoft.EntityFrameworkCore;
+using MaintenanceSystem.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+           builder.Services.AddScoped<IncidentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ MaintenanceSystem/Services/IncidentService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using MaintenanceSystem.Data;
+using MaintenanceSystem.Models;
+
+namespace MaintenanceSystem.Services
+{
+    public class IncidentService
+    {
+        private readonly AppDbContext _context;
+
+        public IncidentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateIncidentAsync(Incident incident)
+        {
+            incident.CreatedAt = DateTime.Now;
+
+            incident.Status = IncidentStatus.Open;
+
+            await _context.Incidents.AddAsync(incident);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
